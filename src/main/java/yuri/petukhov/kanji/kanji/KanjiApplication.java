5cfb974e7967a6be6385d1b4b9f527837925eb74
YuriPetukhov <petuchowjuri@yahoo.de>
2023-11-07 01:06:03 +0300

    Creation of the new project

---
+++ src/main/java/yuri/petukhov/kanji/kanji/KanjiApplication.java
@@ -0,0 +1,13 @@
+package yuri.petukhov.kanji.kanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanjiApplication.class, args);
+	}
+
+}
