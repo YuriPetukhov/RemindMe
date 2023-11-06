3a9aeaf30ddcdf808e266a1cde3686a2b59a547c
YuriPetukhov <petuchowjuri@yahoo.de>
2023-11-07 01:06:29 +0300

    Creation of the new project

---
+++ src/test/java/yuri/petukhov/kanji/kanji/KanjiApplicationTests.java
@@ -0,0 +1,13 @@
+package yuri.petukhov.kanji.kanji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
